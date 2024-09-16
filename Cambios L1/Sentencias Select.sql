d93cd6fb643f6a9e0a071be29ba1f687207d635e
Lenin <lmendez@tecnoav.com.ec>
2024-09-16 14:41:09 -0500

    Carpeta y Archivos Nuevos

---
+++ Cambios L1/Sentencias Select.sql
@@ -0,0 +1,14 @@
+
+
+----------------------------------------------------------------------
+select * from xEstadoFactE where RefNbr = '052345' - '050911'
+select * from xEstadoFactE where RefNbr ='052346'  - '050908'
+select * from xEstadoFactE where RefNbr = '052448' - '052348' ---Rev
+select * from xEstadoFactE where RefNbr = '054105' --- origen error 
+
+
+select * from xEstadoFactE where InvcNbr = '001-002-000011848'
+select * from xEstadoFactE where InvcNbr = '001-002-000011846'
+select * from xEstadoFactE where InvcNbr = '001-002-000012131'
+
+
